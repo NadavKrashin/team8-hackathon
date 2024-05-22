b573d8bb4779184599aefe547215640fb3c4e9c6
yftachsr <94053037+yftachsr@users.noreply.github.com>
2024-05-22 23:49:23 +0300

    profile and file creation

--- Backend/db.py
+++ Backend/db.py
@@ -1,16 +1,38 @@
 import gridfs
 from pymongo import MongoClient
 #prod_url="mongodb://mongodb-service-champion.apps.cluster-4kbfb.4kbfb.sandbox1471.opentlc.com/"
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient('mongodb://admin:password@172.30.8.167:27017/admin')
 db = client['DB']
 profiles = db['profiles']
 fs = gridfs.GridFS(db)
 
 
 def new_profile(profile):
-    profiles.insert_one(profile.dict())
+    lastid_file = open("lastid.txt", 'r+')
+    last_id = lastid_file.read()
+    if last_id == '':
+        last_id_int = 0
+    else:
+        last_id_int = int(last_id)
+    last_id_int += 1
+    lastid_file.seek(0)
+    lastid_file.write(str(last_id_int))
+    lastid_file.truncate()
+    lastid_file.close()
+    profile_dict = profile.dict()
+    profile_dict['id'] = last_id_int
+    profiles.insert_one(profile_dict)
 
-def upload_image(image):
-    fs.put(image)
+
+def get_profile(profile_id):
+    return profiles.find_one({"id": profile_id})
+
+
+def upload_image(image, filename, profile_id):
+    fs.put(image, filename=filename, id=profile_id)
+
+
+def getfile(filename):
+    return fs.find_one({'filename': filename})
 
 
--- Backend/main.py
+++ Backend/main.py
@@ -1,29 +1,29 @@
 from typing import Union
-
 from fastapi import FastAPI, UploadFile
-
 import db
 from Models import Profile
 
 app = FastAPI()
-
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
 
 @app.post("/profiles/new_profile")
 async def new_profile(profile: Profile):
     print(profile)
     db.new_profile(profile)
 
-@app.post("/uploadfile")
-async def create_upload_file(file: UploadFile):
-    db.upload_image(file)
+@app.get("/profiles/{profile_id}")
+async def get_profile(profile_id: int):
+    print(profile_id)
+    return db.get_profile(profile_id)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.post("profiles/{profile_id}/uploadfile")
+async def create_upload_file(file: UploadFile, profile_id: int):
+    #print(file.file.read())
+    db.upload_image(file.file.read(), file.filename, profile_id)
+    #print(db.getfile(file.filename).filename)
+    new_file = open(file.filename, 'wb')
+    new_file.write(db.getfile(file.filename).read())
     return {"filename": file.filename}
