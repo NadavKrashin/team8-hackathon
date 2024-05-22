8379668bad0a0b072d313df78951a41f00842467
nitz121 <117722985+nitz121@users.noreply.github.com>
2024-05-22 18:39:42 +0300

    Update vite.config.js

--- champion/vite.config.js
+++ champion/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8080
+  server:{
+    port:8080
   },
-  plugins:[react()],
-})
+  plugins:[vue()],
+  })
