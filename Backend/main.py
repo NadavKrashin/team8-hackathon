ab6e34ce755b2abcdfb3b0728155645b9fbc2de9
yftachsr <94053037+yftachsr@users.noreply.github.com>
2024-05-23 03:40:19 +0300

    Images functionality

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,4 @@
+import base64
 from typing import Union
 from fastapi import FastAPI, UploadFile
 import db
@@ -6,8 +7,16 @@ from bson import ObjectId
 
 from random import randint
 from re import findall
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/profiles/new_profile")
 async def new_profile(profile: Profile):
@@ -28,27 +37,27 @@ async def get_name(profile_id: str):
 async def get_age(profile_id: str):
     mongo_id = ObjectId(profile_id)
     return str(db.profiles.find_one({"_id": mongo_id})["age"])
-    
+
 @app.get("/profiles/score/{profile_id}")
 async def get_score(profile_id: str):
     mongo_id = ObjectId(profile_id)
     return str(db.profiles.find_one({"_id": mongo_id})["score"])
-    
+
 @app.get("/profiles/hobbies/{profile_id}")
 async def get_hobbies(profile_id: str):
     mongo_id = ObjectId(profile_id)
     return str(db.profiles.find_one({"_id": mongo_id})["hobbies"])
-    
+
 @app.get("/profiles/trophies/{profile_id}")
 async def get_trophies(profile_id: str):
     mongo_id = ObjectId(profile_id)
     return str(db.profiles.find_one({"_id": mongo_id})["trophies"])
-    
+
 @app.get("/profiles/coins/{profile_id}")
 async def get_coins(profile_id: str):
     mongo_id = ObjectId(profile_id)
     return str(db.profiles.find_one({"_id": mongo_id})["coins"])
-    
+
 @app.get("/profiles/gameids/{profile_id}")
 async def get_gameids(profile_id: str):
     mongo_id = ObjectId(profile_id)
@@ -60,21 +69,39 @@ async def read_root():
     db.insert_json()
     return {"Hello": "World"}
 
-@app.post("profiles/{profile_id}/uploadfile")
-async def create_upload_file(file: UploadFile, profile_id: int):
+
+@app.post("/files/uploadfile")
+async def create_upload_file(file: UploadFile, profile_id: int, game_id: int):
     #print(file.file.read())
-    db.upload_image(file.file.read(), file.filename, profile_id)
+    db.upload_image(file.file.read(), file.filename, profile_id, game_id)
     #print(db.getfile(file.filename).filename)
     new_file = open(file.filename, 'wb')
     new_file.write(db.getfile(file.filename).read())
     return {"filename": file.filename}
+
+
+@app.get("/files/get_all")
+async def get_all_images():
+    pictures_dict = {}
+    counter = 0
+    for file in db.get_all_images():
+        picture = {}
+        picture['filename'] = file.filename
+        picture['profile_id'] = file.profile_id
+        picture['verified'] = file.verified
+        picture['game_id'] = file.game_id
+        picture['data'] = base64.b64encode(file.read()).decode('utf-8')
+        pictures_dict[str(counter)] = picture
+        counter += 1
+
+    return pictures_dict
 
 @app.get("/leaderboard")
 async def get_leaderboard(id: str):
     leaderboard = db.profiles.find({}).sort("score", -1).limit(10)
     for doc in leaderboard:
         if doc["_id"] == id:
-            return str(list(leaderboard))        
+            return str(list(leaderboard))
     mongo_id = ObjectId(id)
     return str(list(leaderboard).append(db.profiles.find_one({"_id": mongo_id})))
 
